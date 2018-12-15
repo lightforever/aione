43dd9b3ce41e6d3d31e2833c9421b5902c0d471c
Evgeny Semyonov <lightsanweb@yandex.ru>
2018-12-15 12:38:51 +0300

    adv

--- advanced/model2.py
+++ advanced/model2.py
@@ -10,13 +10,12 @@ def hidden_init(layer):
     lim = 1. / np.sqrt(fan_in)
     return (-lim, lim)
 
-class Actor(nn.Module):
-    """Actor (Policy) Model."""
-
+class BaseModel:s
     def __init__(self, input_size, action_size, seed):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.cnn = MobileNetV2(input_size=input_size)
+        self.cnn.load_state_dict(torch.load(open('mobilenet_v2.pth.tar', 'rb')))
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.reset_parameters()
 
@@ -25,26 +24,17 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        return F.tanh(self.fc3(x))
+        x = F.relu(self.cnn(state))
+        x = F.relu(self.fc1(x))
+        return F.tanh(x)
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    pass
 
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, input_size, action_size, seed):
-        super(Critic, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        pass
-
-    def forward(self, state, action):
-        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))
-        x = torch.cat((xs, action), dim=1)
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+    pass
